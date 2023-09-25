faea6594e0e35cf4c70465d5d88c116a6781df7b
Puneet_bhatnagar <91620854+puneetbhatnagarr@users.noreply.github.com>
2023-09-25 17:32:43 +0530

    Add files via upload

---
+++ Working with Lists, Tuples, and Dictionaries.py
@@ -0,0 +1,30 @@
+# List
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+# tuple
+
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+
+# dict
+
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
---
+++ working with numeric data types.py
@@ -0,0 +1,23 @@
+print(2+2)
+print(4-2)
+print(4*2)
+print(4/2)
+
+myValue=1
+print(myValue)
+
+print(type(myValue))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=3.14
+print(myValue)
+print(type(myValue))
+
+myValue=5j
+print(myValue)
+print(type(myValue))
+
+myValue=True
+print(myValue)
+print(type(myValue))
